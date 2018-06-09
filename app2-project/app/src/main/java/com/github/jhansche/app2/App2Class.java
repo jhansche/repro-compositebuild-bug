a9a9d02cf2b39d25360eaa63c2670bec1e1dc026
Joe Hansche <jhansche@themeetgroup.com>
2018-06-08 19:30:32 -0700

    Try adding an app2 project as well

---
+++ app2-project/app/src/main/java/com/github/jhansche/app2/App2Class.java
@@ -0,0 +1,8 @@
+package com.github.jhansche.app2;
+
+import com.github.jhansche.lib.LibClass;
+
+public class App2Class {
+    public static final String TEST = "app2";
+    public static final String FROM_LIB = "app2+" + LibClass.TEST;
+}
